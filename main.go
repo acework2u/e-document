3999f5c4dcb1e50c8664a262c97852934dc9e208
Anon D <acework2u@gmail.com>
2024-08-29 12:04:45 +0700

    init e-document project

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "GIN OK",
+		})
+	})
+
+	log.Fatal(r.Run(":8088"))
+
+	fmt.Printf("Hello World")
+}
