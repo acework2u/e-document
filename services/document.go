5d2056f8f65f7d58169d044a3ee8450eefcac528
Anon D <acework2u@gmail.com>
2024-10-16 15:43:39 +0700

    add max string verification

--- services/document.go
+++ services/document.go
@@ -36,19 +36,19 @@ type Document struct {
 }
 type DocumentImpl struct {
 	ID             string `json:"id,omitempty"`
-	Year           string `json:"year,omitempty"`
-	RegReceipt     string `json:"regReceipt" binding:"required"`
-	SubjectCode    string `json:"subjectCode" binding:"required"`
-	SubjectType    string `json:"subjectType" binding:"required"`
-	SubjectTitle   string `json:"subjectTitle" binding:"required"`
-	SubjectDetail  string `json:"subjectDetail"`
+	Year           string `json:"year,omitempty,max=4"`
+	RegReceipt     string `json:"regReceipt" binding:"required,max=20" time_format:"2006-01-02"`
+	SubjectCode    string `json:"subjectCode" binding:"required,max=20"`
+	SubjectType    string `json:"subjectType" binding:"required,max=20"`
+	SubjectTitle   string `json:"subjectTitle" binding:"required,max=80"`
+	SubjectDetail  string `json:"subjectDetail" binding:"max=80"`
 	ReceivedDate   string `json:"receivedDate,omitempty" binding:"required"`
-	Sender         string `json:"sender" binding:"required"`
-	Receiver       string `json:"receiver" binding:"required"`
+	Sender         string `json:"sender" binding:"required,max=80"`
+	Receiver       string `json:"receiver" binding:"required,max=80"`
 	ReceivedBy     string `json:"receivedBy"`
-	Status         string `json:"status,omitempty"`
-	DocumentStatus string `json:"documentStatus"`
-	DepartmentCode string `json:"departmentCode"`
+	Status         string `json:"status,omitempty,max=20"`
+	DocumentStatus string `json:"documentStatus,max=20"`
+	DepartmentCode string `json:"departmentCode,max=20"`
 	Acl            []int  `json:"acl,omitempty"`
 	Files          []File `json:"files"`
 	CreatedDate    int64  `json:"createdDate,omitempty"`
